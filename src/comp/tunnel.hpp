30d45b434d93da8ba675bda590a2ed65d98c622c
fenzhu <yheverlove27@outlook.com>
2024-03-07 11:56:07 +0800

    init repo

---
+++ src/comp/tunnel.hpp
@@ -0,0 +1,9 @@
+#ifndef COMP_TUNNEL_HPP
+#define COMP_TUNNEL_HPP
+
+struct HorizontalTunnel
+{
+    float y;
+};
+
+#endif
---
+++ src/core/game.hpp
@@ -0,0 +1,41 @@
+//
+//  game.hpp
+//  EnTT Pacman
+//
+//  Created by Indiana Kernick on 22/9/18.
+//  Copyright © 2018 Indiana Kernick. All rights reserved.
+//
+
+#ifndef CORE_GAME_HPP
+#define CORE_GAME_HPP
+
+#include <random>
+#include "maze.hpp"
+#include <SDL_scancode.h>
+#include "util/sdl_quad_writer.hpp"
+#include <entt/entity/registry.hpp>
+
+class Game {
+public:
+  void init();
+  void input(SDL_Scancode);
+  bool logic();
+  void render(SDL::QuadWriter &, int);
+
+private:
+  enum class State {
+    playing,
+    won,
+    lost
+  };
+
+  entt::registry reg;
+  MazeState maze;
+  int dots = 0;
+  std::mt19937 rand;
+  State state = State::playing;
+  int ticks = 0;
+  bool scattering = true;
+};
+
+#endif
